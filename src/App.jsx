a36b131343cce52df00e6097647ffee0ae88458f
unknown <SenaTarde@ARMAPRCCIFOD587.FormacionSena.RED>
2024-04-16 17:34:34 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { HeaderBlog } from "./components/Layouts/Header/Header"
+import { Section } from "./components/Layouts/Section/Section"
+import { Main } from "./components/Main/Main"
+import { Home } from "./components/pages/Home/Home"
+
+
+function App() {
+  return (
+    <>
+    <Home>
+    <HeaderBlog/>
+    <Main>
+    <Section/>
+    </Main>
+    </Home>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Layouts/Header/Header.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const HeaderBlog = () => <header className='w-full h-32 flex justify-center items-center'><h1 className='text-7xl'>el blog de Uribe</h1></header>
